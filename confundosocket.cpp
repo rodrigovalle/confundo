964b75edfbb1b89c3e50bd2825547b79626edfa9
Rodrigo Valle <rdan.valle@gmail.com>
2017-05-17 09:27:54 -0700

    Added function to report send/recv/drop packets

--- confundosocket.cpp
+++ confundosocket.cpp
@@ -1,11 +1,47 @@
 #include "confundosocket.hpp"
 
 #include <cstring>
+#include <iostream>
 #include <stdexcept>
 #include <string>
 
+enum Operation {
+  SEND,
+  RECV,
+  DROP
+};
+
 static void clear_header(struct cf_packet* pkt) {
   memset(pkt, 0, sizeof(struct cf_header));
+}
+
+static void report(Operation op, struct cf_header* hdr, uint32_t cwnd,
+                   uint32_t ssthresh) {
+  switch (op) {
+    case SEND:
+      std::cout << "SEND ";
+    case RECV:
+      std::cout << "RECV ";
+    case DROP:
+      std::cout << "DROP ";
+  }
+
+  std::cout << hdr->seq << " ";
+  std::cout << hdr->ack << " ";
+  std::cout << hdr->conn << " ";
+  std::cout << cwnd << " ";
+  std::cout << ssthresh;
+
+  if (hdr->ack_f) {
+    std::cout << " ACK";
+  }
+  if (hdr->syn_f) {
+    std::cout << " SYN";
+  }
+  if (hdr->fin_f) {
+    std::cout << " FIN";
+  }
+  std::cout << std::endl;
 }
 
 ConfundoSocket::ConfundoSocket(const std::string& host, const std::string& port)
@@ -18,6 +54,8 @@ ConfundoSocket::ConfundoSocket(const std::string& port) : sock(port) {
 
   // ignore payload (like the linux kernel would)
   sock.recv(reinterpret_cast<uint8_t*>(&rx_hdr), sizeof(struct cf_header));
+  report(RECV, &rx_hdr, 0, 0); // TODO: fill in cwnd and ssthresh
+
   if (!rx_hdr.syn_f) {
     throw std::runtime_error("client sent misconfigured packet");
   }
@@ -27,10 +65,14 @@ ConfundoSocket::ConfundoSocket(const std::string& port) : sock(port) {
   tx_hdr.ack_f = true;
   tx_hdr.ack = rx_hdr.seq + 1;
   tx_hdr.seq = SERVERSYN;
+
   sock.send(reinterpret_cast<uint8_t*>(&tx_hdr), sizeof(struct cf_header));
+  report(SEND, &tx_hdr, 0, 0); // TODO: fill in cwnd and ssthresh
 }
 
-void ConfundoSocket::send_all(const std::string& data) {}
+// TODO: currently blasts UDP packets @server, needs to wait
+void ConfundoSocket::send_all(const std::string& data) {
+}
 
 std::string ConfundoSocket::receive() { return ""; }
 
@@ -43,7 +85,10 @@ void ConfundoSocket::connect() {
 
   // send syn
   sock.send(reinterpret_cast<uint8_t*>(&tx_hdr), sizeof(struct cf_header));
+  report(SEND, &tx_hdr, 0, 0); // TODO: fill in cwnd and ssthresh
+
   sock.recv(reinterpret_cast<uint8_t*>(&rx_hdr), sizeof(struct cf_header));
+  report(RECV, &rx_hdr, 0, 0); // TODO: fill in cwnd and ssthresh
 
   if (!(rx_hdr.syn_f && rx_hdr.ack_f)) {
     throw std::runtime_error("no syn-ack response from server");
